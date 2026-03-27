f3302a1ef07637959a7d3ae89023836789afc65b
Sharrod Skinner <sharrods@gmail.com>
2026-03-27 13:09:47 -0600

    First commit

--- TWN-Jenkins/java-maven-app/script.groovy
+++ TWN-Jenkins/java-maven-app/script.groovy
@@ -2,13 +2,13 @@ def buildJar() {
     echo 'building the application...'
     sh 'mvn package'
 }
-j
+
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t sharroddev/demo-app:jma-1.1 .'
+        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push sharroddev/demo-app:jma-1.1'
+        sh 'docker push nanatwn/demo-app:jma-2.0'
     }
 }
 
