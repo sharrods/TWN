f30eb0ae21acad76646175b12aad0a44546b75fb
sharrods <36249741+sharrods@users.noreply.github.com>
2026-03-29 16:55:25 -0600

    Update Docker image version from 2.0 to 2.1

--- TWN-Jenkins/java-maven-app/script.groovy
+++ TWN-Jenkins/java-maven-app/script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER' )]) {
-        sh 'docker build -t sharrods/demo-app:jma-2.0 -f TWN-Jenkins/java-maven-app/Dockerfile TWN-Jenkins/java-maven-app'
+        sh 'docker build -t sharrods/demo-app:jma-2.1 -f TWN-Jenkins/java-maven-app/Dockerfile TWN-Jenkins/java-maven-app'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push sharrods/demo-app:jma-2.0'
+        sh 'docker push sharrods/demo-app:jma-2.1'
 }
 
 def deployApp() {
