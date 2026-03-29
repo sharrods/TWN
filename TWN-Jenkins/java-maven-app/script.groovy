3402aa55ee6a3302b89cc016afde7405982b3684
sharrods <36249741+sharrods@users.noreply.github.com>
2026-03-29 14:07:08 -0600

    Rename buildJar to buildApp and add testApp function

--- TWN-Jenkins/java-maven-app/script.groovy
+++ TWN-Jenkins/java-maven-app/script.groovy
@@ -1,19 +1,13 @@
-def buildJar() {
+def buildApp() {
     echo 'building the application...'
-    sh 'mvn package'
 }
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
-    }
+def testdApp() {
+    echo 'testing the application...'
 }
 
 def deployApp() {
     echo 'deploying the application...'
+    echo "deploying version ${params.VERSION}"
 }
-
 return this
