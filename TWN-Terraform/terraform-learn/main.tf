b666bb16ba56c20d31cf218f1e18b55e14581e5c
Sharrod Skinner <sharrods@gmail.com>
2026-04-08 06:14:50 -0600

    Removed commented out aws keys and public_key_location, routetable

--- TWN-Terraform/terraform-learn/main.tf
+++ TWN-Terraform/terraform-learn/main.tf
@@ -111,15 +111,6 @@ output "ec2_public_ip" {
   value = aws_instance.myapp-server.public_ip
 }
 
-/*resource "aws_key_pair" "ssh-key" {
-  key_name                = "voip-lab-key"
-  public_key              = "var.my_public_key"
-}*/
-
-/*resource "aws_key_pair" "ssh-key" {
-  key_name                = "voip-lab-key"
-  public_key              = file(var.public_key_location)
-}*/
 
 resource "aws_instance" "myapp-server" {
   ami                     = data.aws_ami.latest-amazon-linux-image.id
@@ -131,15 +122,7 @@ resource "aws_instance" "myapp-server" {
 
   associate_public_ip_address = true 
   key_name                = "voip-lab-key"
-#  key_name                = aws_key_pair.ssh-key.key_name
 
-#/*  user_data = <<EOF
-#                #!/bin/bash/
-#                sudo yum update -y && sudo yum install -y docker
-#                sudo systemctl start docker
-#                sudo usermod -aG docker ec2-user
-#                docker run -p 8080:80 nginx
-#*/                EOF
 
   user_data                   = file("entry-script.sh")
 
