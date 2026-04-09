390aaf838046d1819e906afaa9329205c4d810e8
Sharrod Skinner <sharrods@gmail.com>
2026-04-09 09:37:22 -0600

    Preserve Jenkinsfile with provision server stage with creds

---
+++ TWN-Terraform/java-maven-app/Terraform/variables.tf
@@ -0,0 +1,21 @@
+variable vpc_cidr_block {
+  default             = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+  default             = "10.0.10.0/24"
+}
+variable avail_zone {
+  default             = "us-east-1a"
+}
+variable env_prefix {
+  default             = "dev"
+}
+variable my_ip {
+  default             = "71.205.216.150/32"
+}
+variable instance_type {
+  default = "t3.micro"
+}
+variable region {
+  default = "us-east-1"
+}
