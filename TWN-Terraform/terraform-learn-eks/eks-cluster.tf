2653264635a56d9e034ae2d82e325c67bad51b40
Sharrod Skinner <sharrods@gmail.com>
2026-04-27 08:58:02 -0600

    Add EKS cluster Terraform config - VPC and EKS modules v21 with t3.small node group

--- TWN-Terraform/terraform-learn-eks/eks-cluster.tf
+++ TWN-Terraform/terraform-learn-eks/eks-cluster.tf
@@ -1,53 +1,26 @@
 module "eks" {
-  source                    = "terraform-aws-modules/eks/aws"
-  version                   = "21.17.1"
- 
-  name                      = "myapp-eks-cluster"
-  kubernetes_version        = "1.33"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.17.1"
 
-  subnet_ids                = module.myapp-vpc.private_subnets
-  vpc_id                    = module.myapp-vpc.vpc_id
-  
-  endpoint_public_access    = true
+  name                                     = "myapp-eks-cluster"
+  kubernetes_version                       = "1.33"
+  endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
 
-  addons = {
-    coredns                 = {}
-    eks-pod-identity-agent  = {
-      before_compute = true
-    }
-    kube-proxy              = {}
-    vpc-cni                 = {
-      before_compute = true
-    }
-  }
+  subnet_ids = module.myapp-vpc.private_subnets
+  vpc_id     = module.myapp-vpc.vpc_id
 
-  eks_managed_node_groups   = {
-    dev = {
-      ami_type              = "AL2023_x86_64_STANDARD"
-      instance_types        = ["t3.smal"]
-
-      min_size              = 1
-      max_size              = 3
-      desired_size          = 3
-    }
-  }
-
-  tags                      = {
+  tags = {
     environment = "development"
-    applicaition = "myapp"
+    application = "myapp"
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 3
+      instance_types = ["t3.small"]
+    }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ TWN-Terraform/terraform-learn-eks/eks-cluster_v1.tf.bk
@@ -0,0 +1,53 @@
+module "eks" {
+  source                    = "terraform-aws-modules/eks/aws"
+  version                   = "21.17.1"
+ 
+  name                      = "myapp-eks-cluster"
+  kubernetes_version        = "1.33"
+
+  subnet_ids                = module.myapp-vpc.private_subnets
+  vpc_id                    = module.myapp-vpc.vpc_id
+  
+  endpoint_public_access    = true
+  enable_cluster_creator_admin_permissions = true
+
+  addons = {
+    coredns                 = {}
+    eks-pod-identity-agent  = {
+      before_compute = true
+    }
+    kube-proxy              = {}
+    vpc-cni                 = {
+      before_compute = true
+    }
+  }
+
+  eks_managed_node_groups   = {
+    dev = {
+      ami_type              = "AL2023_x86_64_STANDARD"
+      instance_types        = ["t3.smal"]
+
+      min_size              = 1
+      max_size              = 3
+      desired_size          = 3
+    }
+  }
+
+  tags                      = {
+    environment = "development"
+    applicaition = "myapp"
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
