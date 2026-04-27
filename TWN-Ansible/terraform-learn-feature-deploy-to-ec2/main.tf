368215fbb8e7fe147b4c907b400c1644eebf00c6
Sharrod Skinner <sharrods@gmail.com>
2026-04-26 22:10:49 -0600

    Added 4th server, proper location for ssh key, created new inventoryfile using aws_ec2 plugin to do dynamic inventory

--- TWN-Ansible/terraform-learn-feature-deploy-to-ec2/main.tf
+++ TWN-Ansible/terraform-learn-feature-deploy-to-ec2/main.tf
@@ -57,7 +57,7 @@ resource "aws_default_security_group" "default-sg" {
     protocol = "TCP"
     cidr_blocks = [var.my_ip]
   }
-ŒŒ
+
   ingress {
     from_port = 8080
     to_port = 8080
@@ -124,18 +124,8 @@ resource "aws_instance" "myapp-server-one" {
   }
 }
 
-resource "null_resource" "configure_server" {
-  triggers = {
-    trigger = aws_instance.myapp-server-one.public_ip
-  }
 
-  provisioner "local-exec" {
-    working_dir = "/Users/sharrods/Documents/Techworld-with-nana/TWN-Ansible/ansible-projects"
-    command = "ansible-playbook --inventory ${aws_instance.myapp-server-one.public_ip}, --private-key ${var.ssh_key_private} --vault-password-file ${var.vault_password_file} --user ec2-user deploy-docker-ec2-user.yaml"
-  }
-}
-
-/*resource "aws_instance" "myapp-server-two" {
+resource "aws_instance" "myapp-server-two" {
   ami = data.aws_ami.latest-amazon-linux-image.id
   instance_type = var.instance_type
 
@@ -150,4 +140,35 @@ resource "null_resource" "configure_server" {
     Name: "${var.env_prefix}-server-2"
   }
 }
-*/
+
+resource "aws_instance" "myapp-server-three" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+
+  tags = {
+    Name: "${var.env_prefix}-server-3"
+  }
+}
+
+resource "aws_instance" "myapp-server-four" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+
+  tags = {
+    Name: "${var.env_prefix}-server-4"
+  }
+}
