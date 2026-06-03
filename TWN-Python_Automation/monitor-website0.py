aa15ec449da7564f6cab2bc31662fcfa3a2cc045
Sharrod Skinner <sharrods@gmail.com>
2026-06-03 14:04:02 -0600

    Check docker ps

--- TWN-Python_Automation/monitor-website0.py
+++ TWN-Python_Automation/monitor-website0.py
@@ -1,6 +1,7 @@
 import smtplib
 import requests
 import os
+import paramiko
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
@@ -16,12 +17,22 @@ def send_notification(email_msg):
 
 try:
     response = requests.get('http://74.207.228.174:8080/')
-    if response.status_code == 200:
+    #if response.status_code == 200:
+    if False:
         print('Application is running successfully!')
     else:
         print('Application is Down. Please Fix it!!')
         msg = f"Application returned {response.status_code}"
         send_notification(msg)
+
+        # Restart the application†
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname='74.207.228.174', username='root', key_filename='/Users/sharrods/.ssh/id_ed25519')
+        stdin, stdout, stderr = ssh.exec_command('docker ps')
+        print(stdout.readlines())
+        ssh.close()
+
 except Exception as ex:
     print(f'Connection error happened: {ex}')
     msg = f"Application not accessible at all!!"
