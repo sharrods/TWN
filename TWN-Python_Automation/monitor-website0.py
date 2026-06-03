0f83e06cb176fdfe1af53d96fc1f1e850e38d1ee
Sharrod Skinner <sharrods@gmail.com>
2026-06-03 13:02:53 -0600

    feat: add website monitoring script with email alerting

--- TWN-Python_Automation/monitor-website0.py
+++ TWN-Python_Automation/monitor-website0.py
@@ -1,9 +1,22 @@
+import smtplib
 import requests
+import os
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
 
 response = requests.get('http://74.207.228.174:8080/')
-if response.status_code == 200:
+if False:
     print('Application is running successfully!')
 else:
-    print('Applicatiion is Down. Please Fix it!!')
-    # send email to me!!
+    print('Application is Down. Please Fix it!!')
+    # send email to me
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        msg = "Subject: SITE DOWN\nFix the issue!"
+        smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg )
+
 
