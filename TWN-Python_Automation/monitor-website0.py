e3177173483860cfe24ea64bb59b25191b2b9381
Sharrod Skinner <sharrods@gmail.com>
2026-06-03 14:32:13 -0600

    Added linodeapi4 and reboot linode server

--- TWN-Python_Automation/monitor-website0.py
+++ TWN-Python_Automation/monitor-website0.py
@@ -2,9 +2,12 @@ import smtplib
 import requests
 import os
 import paramiko
+import linode_api4
+
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+LINODE_TOKEN = os.environ.get('LINODE_TOKEN')
 
 def send_notification(email_msg):
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
@@ -17,8 +20,8 @@ def send_notification(email_msg):
 
 try:
     response = requests.get('http://74.207.228.174:8080/')
-    #if response.status_code == 200:
-    if False:
+    if response.status_code == 200:
+    #if False:
         print('Application is running successfully!')
     else:
         print('Application is Down. Please Fix it!!')
@@ -29,9 +32,10 @@ try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(hostname='74.207.228.174', username='root', key_filename='/Users/sharrods/.ssh/id_ed25519')
-        stdin, stdout, stderr = ssh.exec_command('docker ps')
+        stdin, stdout, stderr = ssh.exec_command('docker start 2ff4e1af46f3')
         print(stdout.readlines())
         ssh.close()
+        print('Application restarted')
 
 except Exception as ex:
     print(f'Connection error happened: {ex}')
@@ -39,3 +43,9 @@ except Exception as ex:
     send_notification(msg)
 
 
+    # restart linode server
+    client = linode_api4.LinodeClient(LINODE_TOKEN)
+    nginx_server = client.load(linode_api4.Instance, 98587330 )
+    nginx_server.reboot()
+
+    # restart application
